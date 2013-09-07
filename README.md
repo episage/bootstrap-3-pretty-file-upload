300e625dee807de25f87b313228981b408d37f8d
tomekc <tilosag@gmail.com>
2013-09-07 15:53:15 +0200

    init

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-bootstrap-3-pretty-file-upload
+Pretty file upload button multiselect
 ==============================
 
 Bootstrap 3 pretty file upload
+
+
+  <div class="form-group">
+        <label class="control-label col-lg-3">Pliki</label>
+        <div class="col-lg-9">
+            <div class="prettyFile input-group">
+                <input type="file" name="form[files][]" multiple="multiple">
+                <div class="input-append input-group"">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">Go!</button>
+                    </span>
+                    <input class="input-large form-control" type="text">
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ bootstrap-prettyfile.js
@@ -0,0 +1,29 @@
+﻿// Pretty file
+if ($('.prettyFile').length) {
+    $('.prettyFile').each(function () {
+        var pF = $(this),
+            fileInput = pF.find('input[type="file"]');
+
+        fileInput.change(function () {
+            // When original file input changes, get its value, show it in the fake input
+            var files = fileInput[0].files,
+                info = '';
+            if (files.length > 1) {
+                // Display number of selected files instead of filenames
+                info = files.length + ' files selected';
+            } else {
+                // Display filename (without fake path)
+                var path = fileInput.val().split('\\');
+                info = path[path.length - 1];
+            }
+
+            pF.find('.input-append input').val(info);
+        });
+
+        pF.find('.input-append').click(function (e) {
+            e.preventDefault();
+            // Make as the real input was clicked
+            fileInput.click();
+        })
+    });
+}
